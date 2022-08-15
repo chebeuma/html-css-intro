96b8666c727616cbf15858ac49561b0c09d61550
chebeuma <chebeuma24@gmail.com>
2022-08-15 15:12:03 -0500

    javascript hw7

---
+++ javascript/homework7.js
@@ -0,0 +1,67 @@
+//var ourName
+//console.log(ourName)
+//ourName="uma"
+//console.log(ourName)
+
+
+//1.sum of two numbers
+//function sum(a,b){
+	//return a+b;
+//}
+//console.log(sum(7,2));
+
+
+
+// 2 divide
+//function divide(a,b){
+	//return a/b;
+//}
+//console.log(divide(6,2));
+
+// 3..function fToC() 
+//{
+  //var fTemp = fahrenheit;
+  //var Cel = (fTemp - 32) * 5 / 9;
+//}
+    //console.log(fToC);
+
+//4... divisible by 55
+//function divisibleBy55(num){
+  //return (num % 55 === 0) ? true : false
+//}
+//console.log(divisibleBy55(110))
+
+
+
+
+
+// 5..length of string
+//const checkLength="hello world!";
+//console.log(checkLength.length);
+
+
+
+
+//6.. concation of strings
+  //let str1="cat";
+  //let str2=str1.concat(' dog');
+//console.log(str2);
+
+
+
+
+//let array_Clone = function(arra1) {
+    //return arra1.slice(0);
+      // };
+   //console.log(array_Clone([1, 2, 4, 0]));
+   //console.log(array_Clone([1, 2, [4, 0]]));
+
+
+  //2( let arr1=[1,2,3,4,5,6];
+   //console.log(arr1)
+   //let arr2=[...arr1];
+   //console.log(arr2))
+
+
+
+
