b06221ef339e2117e83e2cf487143b1a8951dbcd
chebeuma <chebeuma24@gmail.com>
2022-09-22 16:32:57 -0700

    pizza
    pizza

--- react-learning/src/components/Pizza/index.jsx
+++ react-learning/src/components/Pizza/index.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+//import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { buyPizza, returnPizza } from "../../redux/pizza/pizzaActions";
 
 const Pizza = () => {
-  const [availableinStore, setAvailable] = useState(10);
+  // const [availableinStore, setAvailable] = useState(10);
+  const availableinstore = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const handleBuypizza = () => {
-    setAvailable((pastAvail) => pastAvail + 1);
+    const action = buyPizza();
+    dispatch(action);
+
+    //setAvailable((pastAvail) => pastAvail + 1);
   };
 
   const handleReturnpizza = () => {
-    setAvailable((pastAvail) => pastAvail - 1);
+    const action = returnPizza();
+    dispatch(action);
+    //setAvailable((pastAvail) => pastAvail - 1);
   };
 
   return (
     <div>
       <h1 className="text-xl m-2">PizzaStore</h1>
-      <h2 className="text-s m-2">AVAILABLE:{availableinStore}</h2>
+      <h2 className="text-s m-2">AVAILABLE:{availableinstore}</h2>
       <button className="border border-black p-1 m-2" onClick={handleBuypizza}>
         decrease by 1
       </button>
--- react-learning/src/redux/store.js
+++ react-learning/src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
+//import balanceReducer from "./balance/balanceReducer";
 import pizzaReducer from "./pizza/pizzaReducer";
-
+//const store = createStore(balanceReducer);
 const store = createStore(pizzaReducer);
-
 export default store;
