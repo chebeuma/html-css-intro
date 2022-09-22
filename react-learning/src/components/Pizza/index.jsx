7d13e1dc2cbc32b440555d3753c004f9e20e37bb
chebeuma <chebeuma24@gmail.com>
2022-09-22 15:42:12 -0700

    pizza
    pizza

--- react-learning/src/components/Pizza/index.jsx
+++ react-learning/src/components/Pizza/index.jsx
@@ -1,21 +1,21 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "../../redux/pizza/pizzaActions";
 
 const Pizza = () => {
-  const availableinStore = useSelector((state) => state);
+  const [availableinStore, setAvailable] = useState(10);
 
   const handleIncrement = () => {
-    //setAvailable((pastAvail) => pastAvail + 1);
+    setAvailable((pastAvail) => pastAvail + 1);
   };
 
   const handleDecrement = () => {
-    //setAvailable((pastAvail) => pastAvail - 1);
+    setAvailable((pastAvail) => pastAvail - 1);
   };
-  console.log(availableinStore);
+
   return (
     <div>
-      <h1 className="text-xl m-2">PizzaInventory</h1>
+      <h1 className="text-xl m-2">PizzaStore</h1>
       <h2 className="text-s m-2">AVAILABLE:{availableinStore}</h2>
       <button className="border border-black p-1 m-2" onClick={handleIncrement}>
         increment by 1
